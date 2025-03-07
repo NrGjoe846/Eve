6936002a9be50b8326af3141b75e4d8d2d82f432
Nehemiah Nesanathan <144255177+NrGjoe846@users.noreply.github.com>
2025-03-07 07:25:58 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
@@ -11,7 +10,7 @@ import PythonFundamentals from './components/courses/PythonFundamentals';
 import CProgramming from './components/courses/CProgramming';
 import JavaProgramming from './components/courses/JavaProgramming';
 import ProgrammingCourses from './components/courses/ProgrammingCourses';
-import CodeMastery from './components/courses/CodeMastery'; // Added import
+import CodeMastery from './components/courses/CodeMastery';
 import ChallengeCategories from './components/challenges/ChallengeCategories';
 import LanguageSelection from './components/challenges/LanguageSelection';
 import LevelSelection from './components/challenges/LevelSelection';
@@ -22,161 +21,164 @@ import AptitudeTest from './components/aptitude/AptitudeTest';
 import About from './components/About';
 import AchievementsPage from './components/achievements/AchievementsPage';
 import RewardsPage from './components/rewards/RewardsPage';
-import Store from './components/Store';
+import StoreTheme from './store/StoreTheme';
+import GameStore from './store/GameStore';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/auth" element={<AuthSystem />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/store" element={<Store />} />
+        <StoreTheme>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/auth" element={<AuthSystem />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/store" element={<GameStore />} />
 
-          {/* Protected Routes */}
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/profile" 
-            element={
-              <ProtectedRoute>
-                <ProfileDashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/compiler" 
-            element={
-              <ProtectedRoute>
-                <CompilerPage />
-              </ProtectedRoute>
-            } 
-          />
-          {/* Achievements */}
-          <Route 
-            path="/achievements" 
-            element={
-              <ProtectedRoute>
-                <AchievementsPage />
-              </ProtectedRoute>
-            } 
-          />
-          {/* Rewards */}
-          <Route 
-            path="/rewards" 
-            element={
-              <ProtectedRoute>
-                <RewardsPage />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Protected Routes */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfileDashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/compiler"
+              element={
+                <ProtectedRoute>
+                  <CompilerPage />
+                </ProtectedRoute>
+              }
+            />
+            {/* Achievements */}
+            <Route
+              path="/achievements"
+              element={
+                <ProtectedRoute>
+                  <AchievementsPage />
+                </ProtectedRoute>
+              }
+            />
+            {/* Rewards */}
+            <Route
+              path="/rewards"
+              element={
+                <ProtectedRoute>
+                  <RewardsPage />
+                </ProtectedRoute>
+              }
+            />
 
-          {/* Courses */}
-          <Route 
-            path="/courses" 
-            element={
-              <ProtectedRoute>
-                <ProgrammingCourses />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/courses/python-fundamentals" 
-            element={
-              <ProtectedRoute>
-                <PythonFundamentals />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/courses/c-programming" 
-            element={
-              <ProtectedRoute>
-                <CProgramming />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/courses/java-programming" 
-            element={
-              <ProtectedRoute>
-                <JavaProgramming />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Courses */}
+            <Route
+              path="/courses"
+              element={
+                <ProtectedRoute>
+                  <ProgrammingCourses />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/courses/python-fundamentals"
+              element={
+                <ProtectedRoute>
+                  <PythonFundamentals />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/courses/c-programming"
+              element={
+                <ProtectedRoute>
+                  <CProgramming />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/courses/java-programming"
+              element={
+                <ProtectedRoute>
+                  <JavaProgramming />
+                </ProtectedRoute>
+              }
+            />
 
-          {/* Challenges */}
-          <Route 
-            path="/challenges" 
-            element={
-              <ProtectedRoute>
-                <ChallengeCategories />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/challenges/language-select" 
-            element={
-              <ProtectedRoute>
-                <LanguageSelection />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/challenges/level-select" 
-            element={
-              <ProtectedRoute>
-                <LevelSelection />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/challenges/daily" 
-            element={
-              <ProtectedRoute>
-                <DailyChallenge />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/challenges/java" 
-            element={
-              <ProtectedRoute>
-                <JavaChallenge />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Challenges */}
+            <Route
+              path="/challenges"
+              element={
+                <ProtectedRoute>
+                  <ChallengeCategories />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/challenges/language-select"
+              element={
+                <ProtectedRoute>
+                  <LanguageSelection />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/challenges/level-select"
+              element={
+                <ProtectedRoute>
+                  <LevelSelection />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/challenges/daily"
+              element={
+                <ProtectedRoute>
+                  <DailyChallenge />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/challenges/java"
+              element={
+                <ProtectedRoute>
+                  <JavaChallenge />
+                </ProtectedRoute>
+              }
+            />
 
-          {/* Interview Bot */}
-          <Route 
-            path="/interview-bot" 
-            element={
-              <ProtectedRoute>
-                <InterviewBot />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Interview Bot */}
+            <Route
+              path="/interview-bot"
+              element={
+                <ProtectedRoute>
+                  <InterviewBot />
+                </ProtectedRoute>
+              }
+            />
 
-          {/* Aptitude Test */}
-          <Route 
-            path="/aptitude-test" 
-            element={
-              <ProtectedRoute>
-                <AptitudeTest />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Aptitude Test */}
+            <Route
+              path="/aptitude-test"
+              element={
+                <ProtectedRoute>
+                  <AptitudeTest />
+                </ProtectedRoute>
+              }
+            />
 
-          {/* Default Route - Redirect to /auth */}
-          <Route path="/" element={<Navigate to="/auth" replace />} />
-        </Routes>
+            {/* Default Route - Redirect to /auth */}
+            <Route path="/" element={<Navigate to="/auth" replace />} />
+          </Routes>
+        </StoreTheme>
       </BrowserRouter>
     </AuthProvider>
   );
